60bca8ded58f97f177f22514289f11cbd524b40f
shubham <design@artificialreality.com>
2024-01-06 15:46:04 +0530

    comments and winning patterns

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const App = {
     },
 
     state :{
-        currentPlayer : 1
+        currentPlayer : 1,
+        moves:[]
     },
     
     init(){
@@ -29,28 +30,48 @@ const App = {
 
         App.$.squares.forEach((square) =>{
             square.addEventListener('click', (event)=>{
-                console.log(`Sqaure with id ${event.target.id} was clicked`);
                 
+                // to check if the sqaure is populated
                 if(square.hasChildNodes()){
                     return
                 }
 
+                // checkin the current player
                 const currentPlayer = App.state.currentPlayer
 
+                // create the div for icons
                 const icon = document.createElement('div');
 
+                // check the icon wrt to the current player
                 if(currentPlayer === 1){
                     icon.classList.add("icon-x");
                 }else {
                     icon.classList.add("icon-o");
                 }
-                
+
+                 
+                // update current player
                 App.state.currentPlayer = App.state.currentPlayer === 1?2:1;
 
+                // add the icon to respective sqaure
                 square.replaceChildren(icon)
+
+                // check for the result of the game
+                const winningPatterns = [
+                    [1,2,3],
+                    [1,5,9],
+                    [1,4,7],
+                    [2,5,8],
+                    [3,5,7],
+                    [3,6,9],
+                    [4,5,6],
+                    [7,8,9],
+                ]
             })
         })      
     }
 }
 
 window,addEventListener("load", App.init);
+
+
